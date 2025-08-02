14736d921c11a10c2a38c1ee7d7be376a94019f7
Jasim9085 <juahmed9085@gmail.com>
2025-08-02 12:19:35 +0530

    Update MainActivity.java

--- app/src/main/java/com/chaquo/python/console/MainActivity.java
+++ app/src/main/java/com/chaquo/python/console/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,14 +13,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        try {
+            setContentView(R.layout.activity_main);
 
-        openNotebookBtn = findViewById(R.id.btn_open_notebook);
-        openNotebookBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, NotebookActivity.class));
-            }
-        });
+            openNotebookBtn = findViewById(R.id.btn_open_notebook);
+            openNotebookBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        Intent intent = new Intent(MainActivity.this, NotebookActivity.class);
+                        startActivity(intent);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity.this, "Failed to open notebook: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Error in MainActivity: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 }
